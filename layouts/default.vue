f12a01c5d84df73218124d54e47b8a58a86cf2a1
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2022-05-14 01:19:18 -0300

    fix, feat: new image, new logo, proyects section, footer section

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,45 +2,61 @@
   <b-container-fluid>
     <Nuxt />
     <br />
+    <div class="footer">
+      <div>
+       	<h5>Redes</h5>
+	<a href="https://github.com/francoZuniga32" target="_blank"><p>Github</p> <img src="/icons/github.png" width="20px"></a>
+	<a href="https://www.linkedin.com/in/franco-agustin-ojeda-zu%C3%B1iga-84826117a/" target="_blank"><p>Linkeding</p><img src="/icons/linkedin.png" width="20px" /></a>
+<a href="https://www.instagram.com/franco.zuniga32/"target="_blank"><p>Instagram</p><img src="/icons/instagram.png"/></a>
+      </div>
+      <div>
+	      <h5>Secciones</h5>
+	      <a href="#sobremi">Sobre Mi</a>
+	      <a href="#trabajos">Trabajos</a>
+	      <a href="proyectos">Proyectos</a>
+      </div>
+      <div>
+	      <h5>Atribuciones</h5>
+      		<div> undefined <a href="https://www.freepik.com" title="Freepik"> Freepik </a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es'</a></div>
+      </div>
+    </div>
   </b-container-fluid>
 </template>
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200;400&display=swap");
-@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
-body {
-  margin: 0;
-  color: black;
+body{
+	scroll-behavior: smooth;
 }
-
-h1, h2, h3, h4, h5{
-  font-family: 'Montserrat', sans-serif;
-}
-
-.navbar {
-  width: 100%;
-  background-color: #3e498c;
-}
-
-.icon {
-  margin: 0 2%;
-}
-.social {
-  width: 100%;
-}
-
-a, a:hover{
-  color: white;
-}
-
 .footer{
-  position: absolute;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
   bottom: 0;
   width: 100%;
+  min-height: 200px;
   padding: 10px;
-  background-color: #6A7FF5;
-  flex-direction: column;
+  background-color: black;
 }
-.footer > div{
-  width: 100%;
+
+.footer div{
+	width: 3fr;
+	height: 100%;
+	display: flex;
+	align-content: space-between;
+	flex-direction: column;
+	justify-content: center;
+  width: 30%;
+  min-width: 300px;
+}
+
+.footer a{
+	width: 90%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+}
+
+.footer a img{
+	width: 20px;
+	height: 20px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,23 @@
   <b-container class="conteiner">
 	<div class="nav-bar">
 		<div class="logo">
-			fn.
+			fz.
 		</div>
 		<div class="links">
 			<ul>
-				<li>Sobre Mi</li>
-				<li>Trabajos</li>
-				<li>Proyectos</li>
+				<li>
+					<a href="#sobremi">Sobre Mi</a>
+				</li>
+				<li>
+					<a href="#trabajos">Trabajos</a>
+				</li>
+				<li>
+					<a href="#proyectos">Proyectos</a>
+				</li>
 			</ul>
 		</div>
 	</div>
-	<div class="contenido">
+	<div class="contenido" id="sobremi">
 		<div class="presentacion">
 			<h1>Hola soy Franco Zuñiga</h1>
 			<p>Soy estudiante en Ciencias de la Computacion, programador web y cofundador de <a>Cooking Beats</a>. Me gusta el mate, emprender nuevos proyectos y ayudar a los demas a adentrarse en el mundo de la progamacion</p>
@@ -21,11 +27,29 @@
 			<img src="/perfil.png"/>
 		</div>
 	</div>
-	<div class="trabajos">
-		<div class="trabajo" v-for="(t, i) in trabajos" :key="i">
-			<h6>{{ t.titulo }}</h6>
-			<p>{{ t.puesto }} - {{t.duracion}}</p>
-			<p>{{ t.descripcion }}</p>
+	<div class="trabajos" id="trabajos">
+		<h3>Trabajos</h3>
+		<div>	
+			<div class="trabajo" v-for="(t, i) in trabajos" :key="i">
+				<h6>{{ t.titulo }}</h6>
+				<p>{{ t.puesto }} - {{t.duracion}}</p>
+				<p>{{ t.descripcion }}</p>
+			</div>
+		</div>
+	</div>
+	<div class="trabajos" id="proyectos">
+		<h3>Proyectos</h3>
+		<div class="proyectos">
+			<div class="proyecto" v-for="(p, i) in proyectos" :key="i">
+				<h6>{{ p.titulo }}</h6>
+				<p>{{ p.descripcion }}</p> 
+				<div>
+					<a v-for="(im, i) in p.links" v-bind:href="im.link">
+						<img v-bind:src="im.img" width="50px"/>
+					</a>
+				</div>
+			</div>
+			<div class="flip"></div>
 		</div>
 	</div>
   </b-container>
@@ -46,6 +70,54 @@ export default{
 					puesto: "Talletista",
 					descripcion: "imparti clases de desarllo de aplicaciones moviles con app inventor, en escuelas secundarias. explicando conceptos de diseño de aplicaciones y programacion",
 					duracion: "Sep 2018 - Jun 2019"
+				},
+				{
+					titulo: "Policia de la Provincia del Neuquen",
+					puesto: "Pasante",
+					descripcion: "realize mis pasantias de 5to año del nivel secundario en la policia de la provincia del neuquen en el dpto de Informatica. Desepeñe mis tareas en el area de programacion.",
+					duracion: "Sep 2016 - Dic 2016"
+				}
+			],
+			proyectos:[
+				{
+					titulo: "S3 server",
+					descripcion: "serviros de expresjs para cargar archivos a s3, sirve como base para cargas en s3",
+					links: [
+						{
+							img: "/icons/github.png",
+							link: "https://github.com/francoZuniga32/s3-server"
+						}
+					]
+				},{
+					titulo: "json2excel",
+					descripcion: "app para convertir excel a json y json a excel en forma de api REST",
+					links: [
+						{
+							img: "/icons/github.png",
+							link: "https://github.com/francoZuniga32/excel2json"
+						},{
+							img: "/icons/docker-hub.png",
+							link: "https://github.com/francoZuniga32/excel2json"
+						},
+					]
+				},{
+					titulo: "Hero bot",
+					descripcion: "hero bot telegram para guardias de sistemas",
+					links: [
+						{
+							img: "/icons/github.png",
+							link: "https://github.com/francoZuniga32/hero-bot"
+						}
+					]
+				},{
+					titulo: "CEFAI rest",
+					descripcion: "aplicacion rest de horarios, correlatividades del centro de estudiantes de la facultad de informatica.",
+					links: [
+						{
+							img: "/icons/github.png",
+							link: "https://github.com/francoZuniga32/CEFAIrest"
+						}
+					]
 				}
 			]
 		}
@@ -58,6 +130,11 @@ body{
 	font-family: 'Ubuntu', sans-serif;
 	color: #C4C4C4;
 	background-color: #292929;
+}
+ 
+a, a:hover{
+	font-family: 'Ubuntu', sans-serif;
+	color: #C4C4C4;
 }
 
 .nav-bar{
@@ -91,31 +168,32 @@ ul li{
 .contenido{
 	width: 100%;
 	margin-top: 10%;
-	display:flex;
-	flex-direction: row;
-	flex-wrap: wrap;
-	justify-content: space-around;
+	display:grid;
+	grid-template-columns: repeat(2, 2fr);
 }
 .presentacion{
-	display: flex;
-	flex-direction: column;
-	width: 40%;
+	grid-template-columns: 2fr;
+	grid-column-gap: 10%;
+	grid-row-gap: 10%;
+	width: 90%;
 	min-width: 300px;
 	border: #C4C4C4 2px solid;
 	border-color: #C4C4C4;
 	padding: 10px;
 }
 .imagen {
-	min-width: 300px;
-	width: 60%;
+	min-width: 300px; 
+	width: 100%;
 	display: flex;
 	flex-direction: row;
 	justify-content: center;
 	align-content: center;
 }
-
 .trabajos{
-	margin-top: 10%;
+	margin-top: 10%
+}
+
+.trabajos div{
 	display: flex;
 	flex-wrap: wrap;
 	flex-direction: row;
@@ -126,11 +204,50 @@ ul li{
 	display:flex;
 	flex-direction: column;
 	max-width: 300px;
+	margin: 10px;
 	border-left: 2px #c4c4c4 solid;
 	padding: 10px;	
 }
 
+
 .imagen img{
-	width: 80%;
+	width: 100%;
+}
+
+.preyectos {
+	display: flex;
+	flex-direction: row;
+}
+
+.proyectos .proyecto{
+	padding: 1%;
+	width: 300px;
+	border-right: 2px solid #c4c4c4;
+	margin: 10px;
+	transition: border linear 1s;
+}
+
+.proyecto:hover {
+	box-shadow: 0px 0px 10px #c4c4c4;
+}
+
+.proyecto div{
+	display:flex;
+	flex-direction: row;
+	flex-wrap: 10%;
+}
+
+
+@media (max-width: 425px){
+	.contenido{
+		grid-template-columns: repeat(1, 1fr);
+	}
+	.contenido .imagen{
+		grid-row-start: 1;
+	}
+
+	.contenidor .presentacion{
+		grid-row-start: 2;
+	}
 }
 </style>
