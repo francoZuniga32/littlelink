fdbf2e2500aba646741220e3219792b08d818e06
franco zuñiga <franco.ojeda@est.fi.uncoma.edu.ar>
2023-11-17 10:36:26 -0300

    nuevos estilos y formto de blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Franco Dev.';
+export const SITE_DESCRIPTION = 'CV y Blog de un programador argentino '; 
---
+++ src/content/blog/nueva.md
@@ -0,0 +1,7 @@
+---
+title: 'SCRUM modelo de desarrollo ... y el negocio?'
+description: 'Here is a sample of some basic Markdown syntax that can be used when writing Markdown content in Astro.'
+pubDate: 'Nov 15 2023'
+heroImage: '/blog-placeholder-1.jpg'
+tags: ["Markdown", "Inicial"]
+---
