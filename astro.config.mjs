d05f8bdd998f8380b9f26aaec27584906d0307a4
franco zuniga <franco.ojeda@est.fi.uncoma.edu.ar>
2025-01-27 00:12:27 -0300

    feat: mdx en astro para poder renderizar el flow component

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  integrations: [react()]
 });
--- src/components/Flow.jsx
+++ src/components/Flow.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { ReactFlow , MiniMap,
     Controls,
     Background} from '@xyflow/react';
- 
 import '@xyflow/react/dist/style.css';
- 
+
+
+import CustomNode from './Flow/CustomNode';
+
+const nodeTypes = { CustomNode: CustomNode };
+
 export default function App() {
   const initialNodes = [
-    { id: '1', position: { x: 0, y: 0 }, data: { label: '22' } },
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '22' }},
     { id: '2', position: { x: 0, y: 100 }, data: { label: '222' } },
   ];
   const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
    
   return (
-    <div style={{ width: '100%', height: '500px',backgroundColor: "white" }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} >
+    <div style={{ width: '100%', height: '500px', backgroundColor: "white", color: "gray" }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} nodeTypes={nodeTypes} >
       <Controls />
         <MiniMap />
         <Background variant="dots" gap={12} size={1} />
