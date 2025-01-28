081e740ed4b3a8a0a7181e6d82ceb03de2463892
franco zuniga <franco.ojeda@est.fi.uncoma.edu.ar>
2025-01-27 21:59:09 -0300

    feat: se compilo y subio el proyecto / 2025-01-27

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,35 @@ rm -r docs/
 
 mv dist/ docs/
 
+mv docs/_astro/ docs/astro/
+
+# cambiamos /_astro/ por /astro/
+
+#!/bin/bash
+
+# Configura la carpeta donde están los archivos .html
+FOLDER_PATH="docs/"
+
+# Cadena que deseas buscar
+SEARCH_STRING="/_astro/"
+
+# Cadena con la que deseas reemplazar
+REPLACE_STRING="/astro/"
+
+# Iterar por todos los archivos .html en la carpeta
+for file in "$FOLDER_PATH"/*.html; do
+    if [[ -f "$file" ]]; then
+        # Usar sed para reemplazar en cada archivo
+        sed -i "s/${SEARCH_STRING}/${REPLACE_STRING}/g" "$file"
+        echo "Procesado: $file"
+    else
+        echo "No se encontró ningún archivo .html en la carpeta."
+    fi
+done
+
+echo "Reemplazo completado."
+
+
 echo "subimos el codigo ..." 
 
 fecha_actual=$(date +"%Y-%m-%d")
